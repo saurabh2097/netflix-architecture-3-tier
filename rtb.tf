17882bc8105741539d5717a77089c4859a230287
saurabh2097 <saurabhchimankar97@gmail.com>
2025-03-28 23:17:39 +0530

    Update rtb.tf

--- rtb.tf
+++ rtb.tf
@@ -23,6 +23,33 @@ resource "aws_route_table_association" "netflix-pub-assoc-2" {
   route_table_id = aws_route_table.netflix-web-rt.id
 }
 
+
+# Create Route Table for Private Subnets
+resource "aws_route_table" "netflix-app-rt" {
+  vpc_id = aws_vpc.netflix-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.netflix-gw.id
+  }
+
+  tags = {
+    Name = "netflix-app-rt"
+  }
+}
+
+# Associate Route Table with Private Subnets
+resource "aws_route_table_association" "netflix-pvt-assoc-1" {
+  subnet_id      = aws_subnet.netflix-pub-sub-1.id
+  route_table_id = aws_route_table.netflix-app-rt.id
+}
+
+resource "aws_route_table_association" "netflix-pvt-assoc-2" {
+  subnet_id      = aws_subnet.netflix-pub-sub-2.id
+  route_table_id = aws_route_table.netflix-app-rt.id
+}
+
+
 # Create a NAT Gateway for Private Subnets
 resource "aws_eip" "netflix-nat-eip" {
   domain = "vpc"
