bb0dffcb65f87eccbfd11698f7bcd21b0640d740
saurabh2097 <saurabhchimankar97@gmail.com>
2025-03-29 18:44:42 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,36 @@
+# Create a Security Group
+resource "aws_security_group" "netflix-web-sg" {
+  name        = "netflix-web-server-sg"
+  description = "Security group for web servers"
+
+  # Allow HTTP traffic from anywhere
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow SSH access from your IP (replace with your IP)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["YOUR_IP/32"]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
