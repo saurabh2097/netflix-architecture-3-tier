156a7e24493f62e767dd00aeb392eaaadc8af41d
saurabh2097 <saurabhchimankar97@gmail.com>
2025-03-29 18:51:15 +0530

    Update webasg.tf

--- webasg.tf
+++ webasg.tf
@@ -5,7 +5,7 @@ launch template{
   id  = aws_launch_configuration.netflix-web_config.id
   version = "$Latest"
 }
- vpc_zone_identifier = [aws_subnet.swiggy-pub-sub-1.id, aws_subnet.swiggy-pub-sub-2.id]
+ vpc_zone_identifier = [aws_subnet.netflix-pub-sub-.id, aws_subnet.netflix-pub-sub2.id]
   min_size             = 2
   max_size             = 3
   desired_capacity     = 2
