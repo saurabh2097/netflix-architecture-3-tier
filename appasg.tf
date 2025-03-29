9d5c31d35c704d717db5eaa86d86b9314906c7aa
saurabh2097 <saurabhchimankar97@gmail.com>
2025-03-29 23:02:37 +0530

    Update appasg.tf

--- appasg.tf
+++ appasg.tf
@@ -1,17 +1,40 @@
-# Create an Auto Scaling Group for Application Server
-resource "aws_autoscaling_group" "app_server_asg" {
-  desired_capacity     = 2
-  max_size             = 3
+# Auto Scaling Group for application server 
+resource "aws_autoscaling_group" "netflix-app-server-asg" {
+name = "netflix-app-asg"
+launch template{
+  id  = aws_launch_configuration.netflix-app_config.id
+  version = "$Latest"
+}
+  vpc_zone_identifier = [
+    aws_subnet.swiggy-pvt-sub-1.id,
+    aws_subnet.swiggy-pvt-sub-2.id
+  ]
   min_size             = 2
-  vpc_zone_identifier  = 
-  launch_template {
-    id      = aws_launch_template.app_server_template.id
-    version = "$Latest"
+  max_size             = 3
+  desired_capacity     = 2
+}
+
+
+
+# Launch Configuration for EC2 Instances for app server
+resource "aws_launch_configuration" "netflix-app-server-asgg" {
+  name          = "netflix-app-template"
+  image_id      = "ami-02f624c08a83ca16f"  
+  instance_type = "t2.micro"
+
+  network_interfaces {
+  security_groups = [aws_security_group.netflix-app-sg-app.id]
+  associate_public_ip_address = false
+}
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+
+    sudo yum install mysql -y
+  EOF
+  )
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
   }
-
-  health_check_type          = "EC2"
-  health_check_grace_period =   300
-  force_delete               = true
-
-  tags = 
 }
