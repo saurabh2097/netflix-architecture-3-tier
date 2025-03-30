2f3c5bef47dd3b017ead5d9d9edcbc72f0937858
saurabh2097 <saurabhchimankar97@gmail.com>
2025-03-30 09:52:50 +0530

    Update webasg.tf

--- webasg.tf
+++ webasg.tf
@@ -1,29 +1,35 @@
-# Auto Scaling Group
+# Auto Scaling Group for web server
 resource "aws_autoscaling_group" "netflix-web-server-asg" {
-name = "swiggy-web-asg"
-launch template{
-  id  = aws_launch_configuration.netflix-web_config.id
-  version = "$Latest"
+  name = "swiggy-web-asg"
+  
+  # Fixing Launch Template reference
+  launch_template {
+    id      = aws_launch_template.netflix-web-template.id
+    version = "$Latest"
+  }
+
+  vpc_zone_identifier = [
+    aws_subnet.netflix-pub-sub-1.id,
+    aws_subnet.netflix-pub-sub-2.id
+  ]
+
+  min_size         = 2
+  max_size         = 3
+  desired_capacity = 2
 }
- vpc_zone_identifier = [aws_subnet.netflix-pub-sub-.id, aws_subnet.netflix-pub-sub2.id]
-  min_size             = 2
-  max_size             = 3
-  desired_capacity     = 2
-}
 
-
-
-# Launch Configuration for EC2 Instances
-resource "aws_launch_configuration" "netflix-web_config" {
+# Launch Template for EC2 Instances
+resource "aws_launch_template" "netflix-web-template" {
   name          = "netflix-web-template"
   image_id      = "ami-02f624c08a83ca16f"  
   instance_type = "t2.micro"
+  
   security_groups = [aws_security_group.netflix-web-sg.id]
 
-  user_data = base64encode(file(netflix.sh))
+  user_data = base64encode(file("netflix.sh"))  # Ensure the file exists
+
   lifecycle {
     prevent_destroy = false
     ignore_changes  = all
-
   }
 }
