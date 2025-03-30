595f6fa794f7e142ffb5d5bc2b69161a0589c89b
saurabh2097 <saurabhchimankar97@gmail.com>
2025-03-30 10:13:39 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "netflix-web-sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["YOUR_IP/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   # Allow all outbound traffic
