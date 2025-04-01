8e65d391bf38d908f1042ed4c32b8f59f164374d
saurabh2097 <saurabhchimankar97@gmail.com>
2025-04-01 13:01:53 +0530

    Update igw.tf

--- igw.tf
+++ igw.tf
@@ -1,6 +1,7 @@
+# Internet Gateway
 resource "aws_internet_gateway" "netflix-igw" {
-  vpc_id = aws_vpc.netflix-vpc.id
   tags = {
     Name = "netflix-igw"
   }
+  vpc_id = aws_vpc.netflix-vpc.id
 }
