7d72b289344c6c391fc1df60c196e38a056311a2
saurabh2097 <saurabhchimankar97@gmail.com>
2025-04-01 12:53:01 +0530

    Update webasg.tf

--- webasg.tf
+++ webasg.tf
@@ -1,36 +1,27 @@
-# Auto Scaling Group for web server
-resource "aws_autoscaling_group" "netflix-web-server-asg" {
-  name = "swiggy-web-asg"
-  
-  # Fixing Launch Template reference
+###### Create an EC2 Auto Scaling Group - web ######
+resource "aws_autoscaling_group" "netflix-web-asg" {
+  name = "netflix-web-asg"
   launch_template {
     id      = aws_launch_template.netflix-web-template.id
     version = "$Latest"
   }
-
-  vpc_zone_identifier = [
-    aws_subnet.netflix-pub-sub-1.id,
-    aws_subnet.netflix-pub-sub-2.id
-  ]
-
-  min_size         = 2
-  max_size         = 3
-  desired_capacity = 2
+  vpc_zone_identifier = [aws_subnet.netflix-pub-sub-1.id, aws_subnet.netflix-pub-sub-2.id]
+  min_size            = 2
+  max_size            = 3
+  desired_capacity    = 2
 }
 
-# Launch Template for EC2 Instances
+###### Create a Launch Template for the EC2 instances ######
 resource "aws_launch_template" "netflix-web-template" {
-  name          = "netflix-web-template"
-  image_id      = "ami-02f624c08a83ca16f"  
+  name_prefix   = "netflix-web-template"
+  image_id      = "ami-0ed194d7eff6d2f81"
   instance_type = "t2.micro"
-key_name      = "saurabhkeypair"
-
-   network_interfaces {
+  key_name      = "saurabhkeypair"
+  network_interfaces {
     associate_public_ip_address = true
-    security_groups = [aws_security_group.netflix-web-sg.id]
+    security_groups             = [aws_security_group.netflix-ec2-asg-sg.id]
   }
-  user_data = base64encode(file("netflix.sh"))  # Ensure the file exists
-
+  user_data = base64encode(file("apache.sh"))
   lifecycle {
     prevent_destroy = false
     ignore_changes  = all
