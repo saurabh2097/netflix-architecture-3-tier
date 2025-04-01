47ede423f47de2a0c5f3b816b8c76b086a77f80c
saurabh2097 <saurabhchimankar97@gmail.com>
2025-04-01 13:05:07 +0530

    Update appasg.tf

--- appasg.tf
+++ appasg.tf
@@ -1,15 +1,15 @@
-# Auto Scaling Group for Application Server
-resource "aws_autoscaling_group" "netflix-app-server-asg" {
+# Create an EC2 Auto Scaling Group - app
+resource "aws_autoscaling_group" "netflix-app-asg" {
   name = "netflix-app-asg"
-  
+
   launch_template {
-    id      = aws_launch_template.netflix-app-server-template.id
+    id      = aws_launch_template.netflix-app-template.id
     version = "$Latest"
   }
 
   vpc_zone_identifier = [
-    aws_subnet.netflix-priv-sub-1.id,
-    aws_subnet.netflix-priv-sub-2.id
+    aws_subnet.netflix-pvt-sub-1.id,
+    aws_subnet.netflix-pvt-sub-2.id
   ]
 
   min_size         = 2
@@ -17,20 +17,21 @@ resource "aws_autoscaling_group" "netflix-app-server-asg" {
   desired_capacity = 2
 }
 
-# Launch Template for EC2 Instances for App Server
-resource "aws_launch_template" "netflix-app-server-template" {
-  name          = "netflix-app-template"
+# Create a launch template for the EC2 instances
+resource "aws_launch_template" "netflix-app-template" {
+  name_prefix   = "netflix-app-template"
   image_id      = "ami-02f624c08a83ca16f"
   instance_type = "t2.micro"
   key_name      = "saurabhkeypair"
 
   network_interfaces {
-    security_groups             = [aws_security_group.netflix-app-sg-app.id]
+    security_groups             = [aws_security_group.netflix-ec2-asg-sg-app.id]
     associate_public_ip_address = false
   }
 
   user_data = base64encode(<<-EOF
     #!/bin/bash
+
     sudo yum install mysql -y
   EOF
   )
