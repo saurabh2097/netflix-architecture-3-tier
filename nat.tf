672faaaf1cb87932a69f5c49c4f2cfc22ad7b8d1
saurabh2097 <saurabhchimankar97@gmail.com>
2025-04-01 12:58:24 +0530

    Update nat.tf

--- nat.tf
+++ nat.tf
@@ -1,10 +1,10 @@
-# Create a NAT Gateway
-resource "aws_nat_gateway" "netflix-nat-gateway" {
+#NatGW
+resource "aws_nat_gateway" "netflix-natgw-01" {
   allocation_id = aws_eip.netflix-nat-eip.id
   subnet_id     = aws_subnet.netflix-pub-sub-1.id
 
   tags = {
     Name = "netflix-natgw-01"
   }
-depends_on = [aws_internet_gateway.netflix-gw]
+  depends_on = [aws_internet_gateway.netflix-igw]
 }
