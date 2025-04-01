cdc695d89a0153384819440a6409aa718f0fb9bb
saurabh2097 <saurabhchimankar97@gmail.com>
2025-04-01 13:03:44 +0530

    Update applb.tf

--- applb.tf
+++ applb.tf
@@ -1,23 +1,23 @@
-# Create an Application Load Balancer
-resource "aws_lb" "netflix-app-lb-external" {
-  name               = "netflix-app-lb"
+# Create a Load Balancer
+resource "aws_lb" "external" {
+  name               = "netflix-lb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.netflix-sg-lb.id]
+  security_groups    = [aws_security_group.netflix-alb-sg-1.id]
   subnets            = [aws_subnet.netflix-pub-sub-1.id, aws_subnet.netflix-pub-sub-2.id]
-  enable_deletion_protection = false
 
+  enable_deletion_protection = false
 }
 
-# Target Group for App Servers
-resource "aws_lb_target_group" "app_target_group" {
-  name     = "netflix-app-target-group"
+# Create a Target Group
+resource "aws_lb_target_group" "external-elb" {
+  name     = "external-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.netflix-vpc.id
 
   health_check {
-    path                = "/health"
+    path                = "/"
     interval            = 30
     timeout             = 5
     healthy_threshold   = 3
@@ -26,7 +26,7 @@ resource "aws_lb_target_group" "app_target_group" {
 }
 
 # Attach Auto Scaling Group Instances to Target Group
-resource "aws_autoscaling_attachment" "netflix-app-asg-attachment" {
-  autoscaling_group_name = aws_autoscaling_group.netflix-app-server-asg.name
-  lb_target_group_arn    = aws_lb_target_group.app_target_group.arn
+resource "aws_autoscaling_attachment" "swiggy-web-asg-attachment" {
+  autoscaling_group_name = aws_autoscaling_group.netflix-web-asg.name
+  lb_target_group_arn    = aws_lb_target_group.external-elb.arn
 }
