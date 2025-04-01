90c48a90074b35255be53f473a2f9ed1f5e64772
saurabh2097 <saurabhchimankar97@gmail.com>
2025-04-01 19:23:27 +0530

    Update db.tf

--- db.tf
+++ db.tf
@@ -1,28 +1,27 @@
 #### RDS ####
-/*
-resource "aws_db_subnet_group" "netflix-db-sub-grp" {
-  name       = "netflix-db-sub-grp"
-  subnet_ids = ["${aws_subnet.netflix-db-sub-1.id}","${aws_subnet.netflix-db-sub-2.id}"]
+/*resource "aws_db_subnet_group" "swiggy-db-sub-grp" {
+  name       = "swiggy-db-sub-grp"
+  subnet_ids = ["${aws_subnet.swiggy-pvt-sub-3.id}","${aws_subnet.swiggy-pvt-sub-4.id}"]
 }
 
-resource "aws_db_instance" "netflix-db" {
+resource "aws_db_instance" "swiggy-db" {
   allocated_storage           = 100
   storage_type                = "gp3"
   engine                      = "mysql"
   engine_version              = "8.0.39"
   instance_class              = "db.m5.large"
-  identifier                  = "netflix-db"
+  identifier                  = "swiggy-db"
   username                    = "admin"
-  password                    = "Devopsbysaurabh20*"
+  password                    = "Devopsbyraham007*"
   parameter_group_name        = "default.mysql8.0"
-  db_subnet_group_name        = aws_db_subnet_group.netflix-db-sub-grp.name
-  vpc_security_group_ids      = ["${aws_security_group.netflix-db-sg.id}"]
+  db_subnet_group_name        = aws_db_subnet_group.swiggy-db-sub-grp.name
+  vpc_security_group_ids      = ["${aws_security_group.swiggy-db-sg.id}"]
   multi_az                    = true
   skip_final_snapshot         = true
   publicly_accessible          = false
 
   lifecycle {
-    prevent_destroy = false
+    prevent_destroy = true
     ignore_changes  = all
   }
 }
