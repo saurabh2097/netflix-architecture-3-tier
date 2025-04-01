0501220142a2b50cbf93d0c94a0c616dd7e0fa75
saurabh2097 <saurabhchimankar97@gmail.com>
2025-04-01 12:56:00 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,22 +1,9 @@
-# Create a Security Group for web ASG
-resource "aws_security_group" "netflix-web-sg" {
-  name        = "netflix-web-server-sg"
-  description = "Security group for web servers"
- vpc_id   = aws_vpc.netflix-vpc.id
+# Security Group for ASG
+resource "aws_security_group" "netflix-ec2-asg-sg" {
+  name        = "netflix-ec2-asg-sg"
+  description = "Security group for EC2 instances in ASG"
+  vpc_id      = aws_vpc.netflix-vpc.id
 
-  # Allow HTTP traffic from anywhere
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   ingress {
     from_port   = 80
     to_port     = 80
@@ -24,91 +11,29 @@ resource "aws_security_group" "netflix-web-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Allow all outbound traffic
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-tags = {
-    Name = "Netflix-web-server-sg"
-  }
-}
 
-#------ Create a Security Group for Load Balancer
-resource "aws_security_group" "netflix-sg-lb" {
-  name        = "netflix-alb-security-group"
-  description = "Security group for Application Load Balancer to allow HTTP and HTTPS traffic"
- vpc_id   = aws_vpc.netflix-vpc.id
-
- ingress {
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-
- ingress {
-  from_port   = 22
-  to_port     = 22
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-
-ingress {
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
- cidr_blocks = ["0.0.0.0/0"]
-}
-
-
-  # Allow all outbound traffic (default behavior)
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1" # -1 represents all protocols
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   tags = {
-    Name = "Netflix-alb-security-group"
-  }
-}
-
-
-# Create a Security Group for Application Server-----
-resource "aws_security_group" "netflix-app-sg-app" {
-  name        = "netflix-app-server-sg"
-  description = "Allow HTTP and SSH traffic"
-   vpc_id   = aws_vpc.netflix-vpc.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP traffic from anywhere
-  }
-ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP traffic from anywhere
-  }
-ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-tags = {
-    Name = "Netflix-app-server-sg"
+    Name = "netflix-ec2-asg-sg"
   }
 }
 
@@ -116,8 +41,8 @@ tags = {
 # Security Group for Database
 resource "aws_security_group" "netflix-db-sg" {
   name        = "netflix-db-sg"
-  description = "Security group for Swiggy Database"
-   vpc_id   = aws_vpc.netflix-vpc.id
+  description = "netflix group for netflix Database"
+  vpc_id      = aws_vpc.netflix-vpc.id
 
   ingress {
     from_port = 3306 # MySQL/Aurora port
@@ -140,6 +65,77 @@ resource "aws_security_group" "netflix-db-sg" {
   }
 
   tags = {
-    Name = "Netflix-db-sg"
+    Name = "netflix DB SG"
+  }
+}
+
+# Security Group for Application Load Balancer
+resource "aws_security_group" "netflix-alb-sg-1" {
+  name        = "netflix-alb-sg-1"
+  description = "Security group for netflix Application Load Balancer"
+  vpc_id      = aws_vpc.netflix-vpc.id
+
+  ingress {
+    from_port   = 80 # HTTP
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443 # HTTPS
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "netflix ALB SG"
+  }
+}
+
+# Create a Security Group for ASG
+resource "aws_security_group" "netflix-ec2-asg-sg-app" {
+  name        = "netflix-ec2-asg-sg-app"
+  description = "Security group for netflix EC2 instances in ASG"
+  vpc_id      = aws_vpc.netflix-vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "netflix-ec2-asg-sg-app"
   }
 }
